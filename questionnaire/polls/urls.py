26772d2d2853efb83f2eccc71d396cf06754f34d
A-miin <zulaykaisaeva@gmail.com>
2021-04-03 12:45:26 +0600

    Add detailview for poll

--- questionnaire/polls/urls.py
+++ questionnaire/polls/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views import (
     IndexPollView,
+    ViewPollView,
 )
 
 urlpatterns=[
     path('',IndexPollView.as_view() , name = 'poll-list'),
+    path('poll/<int:pk>', ViewPollView.as_view(), name = "poll-view")
 
 ]
