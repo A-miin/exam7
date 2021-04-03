4d3923722f59774e7b7d798f2fc726f9f78ad156
A-miin <zulaykaisaeva@gmail.com>
2021-04-03 12:11:48 +0600

    Add models, register them

--- questionnaire/polls/admin.py
+++ questionnaire/polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Poll, Choice
 
 # Register your models here.
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question']
+    fields = ['id', 'question', 'created_at']
+    readonly_fields = ['id', 'created_at']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id','option']
+    fields = ['id', 'option', 'poll']
+    readonly_fields = ['id']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+
--- questionnaire/polls/models.py
+++ questionnaire/polls/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    question = models.TextField(max_length=256, null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name="Вопрос"
+        verbose_name_plural="Вопросы"
+
+    def __str__(self):
+        return self.question
+
+class Choice(models.Model):
+    option = models.TextField(max_length=256, null=False, blank=False)
+    poll = models.ForeignKey('polls.Poll', on_delete=models.CASCADE, related_name="choices", verbose_name="Ответы")
+
+    class Meta:
+        verbose_name="Ответ"
+        verbose_name_plural="Ответы"
+
+    def __str__(self):
+        return f'{self.poll.question}:{self.option}'
